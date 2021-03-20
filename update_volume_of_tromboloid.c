64ba63b89194f0429d2a664629c4c899e903d7a8
ENG19CS0021_AKSHAT MISHRA <76100573+ENG19CS0021@users.noreply.github.com>
2021-03-20 18:57:39 +0530

    Update update_volume_of_tromboloid.c

--- update_volume_of_tromboloid.c
+++ update_volume_of_tromboloid.c
@@ -8,7 +8,7 @@ v=((h*b*d)+(d/b))/3;
 printf(“volume of tromboloid is :%f\n”,v);
 return 0;
   
-   float h,b,d,v;
+    float h,b,d,v;
    printf("\nEnter the value of h,b and d \n");
    scanf("%f%f%f",&h,&b, &d);
    v=((h*b*d)+(d/b))/3;
