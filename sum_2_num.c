9a4e924f08aeac7a3134d29a754ea0d591ec2d15
ENG19CS0021_AKSHAT MISHRA <76100573+ENG19CS0021@users.noreply.github.com>
2021-03-20 18:20:07 +0530

    Create sum_2_num.c

---
+++ sum_2_num.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+printf(“enter the value of a :”); // Enter number: 1
+scanf(“%d”, &a);
+printf(“enter the value of b: ”); //Enter number:2
+scanf(“%d”,&b);
+c=a+b;
+printf(“the sum of a and b is %d”, c);// the sum of 1 and 2 is 3
+return 0;
+   int a,b,c;
+    printf("enter value of a"); //enter number=1
+    scanf("%d",&a);
+    printf("enter value of b"); //enter number=2
+    scanf("%d",&b);
+    c=a+b;
+    printf("the sum of a and b is %d",c); // sum of 1 and 2 is 3
+    return 0;
+  
+}
+
