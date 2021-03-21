a55e841c181659867bb2dcd4ab83dc16296f20a3
ENG19CS0021_AKSHAT MISHRA <76100573+ENG19CS0021@users.noreply.github.com>
2021-03-21 19:38:28 +0530

    Update distance_2_points_4_func.c

--- distance_2_points_4_func.c
+++ distance_2_points_4_func.c
@@ -31,36 +31,3 @@ int main()
     output(z,x1,y1,x2,y2);
     return 0;
 }
-include<stdio.h>
-include<math.h>
-float input()
-{
-float a;
-scanf(“%f”,&a);
-return a;
-}
-float distance(float x1, float y1, float x2, float y2)
-{
-float dis;
-dis=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-return dis;
-}
-void output(float x1, float y1, float x2, float y2,float dis)
-{
-printf("The distance between %f,%f and %f,%f is %f\n",x1,y1,x2,y2,dis);
-}
-Int main()
-{
-float x1,x2,y1,y2,z;
-printf(“Enter x1 coordinate\n”);
-x1=input();
-printf(“Enter y1 coordinate\n”);
-y2=input();
-printf(“Enter x2 coordinate\n”);
-x2=input();
-printf(“Enter y2 coordinate\n”);
-y2=input();
-z=distance(x1,y1,x2,y2);
-output(z,x1,y1,x2,y2);
-Return 0;
-}
