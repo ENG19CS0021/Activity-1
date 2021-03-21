00f7645d532faf449eb547b510814745ea162e2c
ENG19CS0021_AKSHAT MISHRA <76100573+ENG19CS0021@users.noreply.github.com>
2021-03-21 18:39:07 +0530

    Update vol_tromboloid_using_4_function.c

--- vol_tromboloid_using_4_function.c
+++ vol_tromboloid_using_4_function.c
@@ -1,30 +1,31 @@
 #include<stdio.h>
+
 float input()
 {
-float a;
-scanf(“%f”,&a);
-return a;
+    float a;
+    scanf("%f",&a);
+    return a;
 }
 float find_vol(float a, float b, float c)
 {
-float vol;
-vol = ((a*b*c)+(b/c))/3.0;
-return vol;
+    float vol;
+    vol=((a*b*c)+(b/c))/3.0;
+    return vol;
 }
 void output(float a)
 {
-printf(“the result is %f”,a);
+    printf("the ans is %f",a);
 }
 int main()
 {
-float x,y,z,vol;
-printf(“enter the height\n”);
-x=input();
-printf(“enter the depth\n”);
-y=input();
-printf(“enter the breadth\n”);
-z=input();
-vol=find_vol(x,y,z);
-output(vol);
-return 0;
+    float x,y,z,vol;
+    printf("enter the height\n");
+    x=input();
+    printf("enter the depth\n");
+    y=input();
+    printf("enter the breadth\n");
+    z=input();
+    vol=find_vol(x,y,z);
+    output(vol);
+    return 0;
 }
