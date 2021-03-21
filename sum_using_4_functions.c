90bd86a029f971133053aaec27342903db06cd8b
ENG19CS0021_AKSHAT MISHRA <76100573+ENG19CS0021@users.noreply.github.com>
2021-03-21 18:40:17 +0530

    Update sum_using_4_functions.c

--- sum_using_4_functions.c
+++ sum_using_4_functions.c
@@ -1,30 +1,4 @@
-#include<stdio.h>
-int input()
-{
-int a;
-printf(“Enter a number\n”);
-scanf(“%d”,&a);
-return a;
-}
-int find_sum(int a, int b)
-{
-int sum;
-sum=a+b;
-return sum;
-}
-void output(int a, int b, int c)
-{
-printf(“the sum of %d and %d is %d\n”,a, b, c);
-}
-Int main()
-{
-int x,y,z;
-x=input();
-y=input();
-z=find_sum(x,y);
-output(x,y,z);
-return 0;
-}
+
 #include<stdio.h>
 int input()
 {
