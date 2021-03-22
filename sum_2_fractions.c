e931d08f91c8b735954a49c8b5d4f8f7966372fb
ENG19CS0021_AKSHAT MISHRA <76100573+ENG19CS0021@users.noreply.github.com>
2021-03-22 15:25:49 +0530

    Update sum_2_fractions.c

--- sum_2_fractions.c
+++ sum_2_fractions.c
@@ -1,40 +1,42 @@
 #include<stdio.h>
 struct fract
 {
-int nume;
-Int deno;
+    int nume;
+    int deno;
 };
 typedef struct fract fraction;
 fraction input()
 {
-fraction a;
-printf(“Enter the numerator”);
-scanf(“%d”, &(a.nume));
-printf(“Enter the denominator”);
-scanf(“%d”, &(a.deno));
-return a;
+    fraction a;
+    printf("enter the numerator\n ");
+    scanf("%d",&(a.nume));
+    printf("enter the denominator\n ");
+    scanf("%d",&(a.deno));
+    return a;
 }
 fraction sum(fraction a, fraction b)
 {
-fraction res;
-if(a.deno==b.deno)
-{
-res.deno=a.deno;
-res.nume=a.nume+b.nume;
+    fraction res;
+    if(a.deno==b.deno)
+    {
+        res.deno=a.deno;
+        res.nume=a.nume+b.nume;
+    }
+    else
+    {
+        res.deno=a.deno*b.deno;
+        res.nume=(a.nume*b.deno)+(b.nume*a.deno);
+    }
+    return res;
 }
-else 
-{
-res.deno=a.deno*b.deno;
-res.nume=(a.nume*b.deno)+(b.nume*a.deno);
-}
-return 0;
-}
+
 int main()
 {
-fraction a,b,c;
-a=input();
-b=input();
-c=sum(a,b);
-printf(“The final sum is %d/%d”,c.nume,c.deno);
-return 0;
+    fraction a,b,c;
+    a=input();
+    b=input();
+    c=sum(a,b);
+    printf("the final sum is %d / %d",c.nume,c.deno);
+    return 0;
 }
+
