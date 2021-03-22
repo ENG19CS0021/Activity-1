775e0b6f0d322c137972aa3041ba8d4c442efdb5
ENG19CS0021_AKSHAT MISHRA <76100573+ENG19CS0021@users.noreply.github.com>
2021-03-22 16:03:24 +0530

    Update sum_n_numbers.c

--- sum_n_numbers.c
+++ sum_n_numbers.c
@@ -13,3 +13,18 @@ sum=sum+a[i];
 printf(“the total sum is %d”, sum);
 Return 0;
 }
+#include<stdio.h>
+int main()
+{
+    int n,a[10],sum=0;
+    printf("Enter total number\n");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+     printf("Enter a number\n");
+     scanf("%d",&a[i]);
+     sum=sum+a[i];
+    }
+    printf("The total sum is %d",sum);
+    return 0;
+}
