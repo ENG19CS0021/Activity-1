a6e271a12656e9df1f5c8884c8fdc80a4a970884
ENG19CS0021_AKSHAT MISHRA <76100573+ENG19CS0021@users.noreply.github.com>
2021-03-23 11:49:28 +0530

    Update sum_2_num.c

--- sum_2_num.c
+++ sum_2_num.c
@@ -1,15 +1,3 @@
-#include<stdio.h>
-int main()
-{
-int a,b,c;
-printf(“enter the value of a :”); // Enter number: 1
-scanf(“%d”, &a);
-printf(“enter the value of b: ”); //Enter number:2
-scanf(“%d”,&b);
-c=a+b;
-printf(“the sum of a and b is %d”, c);// the sum of 1 and 2 is 3
-return 0;
-}
 #include <stdio.h>
 int main()
 {
