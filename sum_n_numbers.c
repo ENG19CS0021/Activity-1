ce17610d577ecea1ec5e7a840c7baef7710d7b22
ENG19CS0021_AKSHAT MISHRA <76100573+ENG19CS0021@users.noreply.github.com>
2021-03-31 23:01:20 +0530

    Update sum_n_numbers.c

--- sum_n_numbers.c
+++ sum_n_numbers.c
@@ -1,15 +1,33 @@
-#include<stdio.h>
+#include <stdio.h>
+
+void input(float a[],int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        printf("Enter %d number", (i+1));
+        
+        scanf("%f",&a[i]);
+    }
+}
+float sum(float a[],int n)
+{
+    float s=0.0;
+    for(int i=0; i<n; i++)
+    s=s+a[i];
+    return s;
+}
+void output(float s)
+{
+    printf("The sum of numbers are %f",s);
+}
 int main()
 {
-    int n,a[10],sum=0;
-    printf("Enter total number\n");
+    int n;
+    printf("Enter the total number of numbers\n");
     scanf("%d",&n);
-    for(int i=0;i<n;i++)
-    {
-     printf("Enter a number\n");
-     scanf("%d",&a[i]);
-     sum=sum+a[i];
-    }
-    printf("The total sum is %d",sum);
+    float a[n];
+    input(a,n);
+    float s=sum(a,n);
+    output(s);
     return 0;
 }
