5f57224826ae1d6a4595b6ead50da62938044474
ENG19CS0021_AKSHAT MISHRA <76100573+ENG19CS0021@users.noreply.github.com>
2021-04-02 00:36:23 +0530

    Update sum_2_fractions.c

--- sum_2_fractions.c
+++ sum_2_fractions.c
@@ -20,7 +20,7 @@ int gcd(int a, int b)
     int g=1;
     for(int i=2;i<=a && i<=b;i++)
     {
-        if(a%i==0 && i<=b; i++)
+        if(a%i==0 && b%i==0)
          { 
             g=i;
          }
